3c9b1a25964b63338d36f6d75ba7b92e86a33bb7
Juli Patrusheva <midimonster@julipatrusheva.com>
2020-11-15 20:39:37 +0100

    fix resposive disable and enable of the animation block

--- theme/static/js/custom.js
+++ theme/static/js/custom.js
@@ -2,27 +2,13 @@ var animationXWords = ["control", "connect"]
 var animationYWords = ["using", "to"]
 
 document.addEventListener("DOMContentLoaded", () => {
-	var wordX = document.getElementsByClassName("saying").item(0);
-	var wordY = document.getElementsByClassName("saying").item(1);
-	randomConnectionID = getRandomInt(animationXWords.length);
-	wordX.innerHTML = "<p>" + animationXWords[randomConnectionID] + "</p>"
-	wordY.innerHTML = "<p>" + animationYWords[randomConnectionID] + "</p>"
+	var usageExamplesElement = document.getElementById("usage-examples");
+	var usageExamplesElementHTML = usageExamplesElement.cloneNode(true);
+	updateUsageBlock(usageExamplesElement, usageExamplesElementHTML);
+	window.addEventListener('resize', ()=> {
+		updateUsageBlock(usageExamplesElement, usageExamplesElementHTML);
+	});
 
-	clearAnimations("list-from");
-	clearAnimations("list-to");
-
-	var listX = document.querySelector(".list-from");
-	console.log(listX.children.length);
-	var listY = document.querySelector(".list-to");
-	//shuffleNodes(listX);
-	//shuffleNodes(listY);
-	window.setInterval(function () {
-		changeListPosition(listX, getRandomInt(listX.firstElementChild.children.length))
-	}, 2000);
-	window.setInterval(function () {
-		changeListPosition(listY, getRandomInt(listY.firstElementChild.children.length))
-	}, 2000);
-	
   });
 
 function clearAnimations(classN) {
@@ -40,3 +26,43 @@ function changeListPosition(list, itemNumber) {
 	list.firstElementChild.children[itemNumber].querySelector("p").style = "color:#23cb97;";
 	
 }
+
+function insertAfter(newNode, existingNode) {
+    existingNode.parentNode.insertBefore(newNode, existingNode.nextSibling);
+}
+
+function updateUsageBlock(usageExamplesElement, usageExamplesElementHTML){
+	if (window.screen.width<768) {
+		document.getElementById("usage-examples").parentNode.removeChild(usageExamplesElement);
+	} else {
+		if (document.getElementById("usage-examples")==null) {
+			insertAfter(usageExamplesElementHTML, document.getElementById("multi-protocol-translation-software"));
+		}
+		var wordX = document.getElementsByClassName("saying").item(0);
+		var wordY = document.getElementsByClassName("saying").item(1);
+		randomConnectionID = getRandomInt(animationXWords.length);
+		wordX.innerHTML = "<p>" + animationXWords[randomConnectionID] + "</p>"
+		wordY.innerHTML = "<p>" + animationYWords[randomConnectionID] + "</p>"
+
+		clearAnimations("list-from");
+		clearAnimations("list-to");
+
+		var listX = document.querySelector(".list-from");
+		console.log(listX.children.length);
+		var listY = document.querySelector(".list-to");
+		//shuffleNodes(listX);
+		//shuffleNodes(listY);
+		window.setInterval(function () {
+			changeListPosition(listX, getRandomInt(listX.firstElementChild.children.length))
+		}, 2000);
+		window.setInterval(function () {
+			changeListPosition(listY, getRandomInt(listY.firstElementChild.children.length))
+		}, 2000);
+	}
+}
+
+var elems = document.querySelectorAll(".widget.hover");
+
+[].forEach.call(elems, function(el) {
+    el.classList.remove("hover");
+});
